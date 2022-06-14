cf3cbfc1006d31b41f570149861ea690edba17c4
Peter Ndumia <ndegwandumia@gmail.com>
2022-06-14 20:50:38 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -12,5 +12,26 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  let tutorials = [
+    'what does the this keyword mean?',
+    'What is the Constructor OO pattern?',
+    'implementing Blockchain Web API',
+    'The Test Driven Development Workflow',
+    'What is NaN and how Can we Check for it',
+    'What is the difference between stopPropagation and preventDefault?',
+    'Immutable State and Pure Functions',
+    'what is the difference between == and ===?',
+    'what is the difference between event capturing and bubbling?',
+    'what is JSONP?'
+  ];
+   tutorials = tutorials.map(tutorialsItem => {
+         
+    return tutorialsItem.split(' ').map(word => word.charAt(0).toUpperCase()+word.slice(1)).join(' ');
+
+});
+
+ 
+  return tutorials ;
 }
+
+
